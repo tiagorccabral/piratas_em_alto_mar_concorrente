daf031b9f516174bc55fc9172531cb58d26e2b2a
Tiago Cabral <tiagorc.cabral@gmail.com>
2019-05-30 11:28:56 -0300

    Adiciona forcas as tripulacoes de piratas e rei piratas

--- tp1_150150296.c
+++ tp1_150150296.c
@@ -18,6 +18,12 @@
         Os marinheiros tem objetivo de expulsar os piratas da ilha.
 */
 
+#if defined(_WIN32)
+    #define CLEAR system("cls");
+#else
+    #define CLEAR system("clear");
+#endif
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <pthread.h>
@@ -25,7 +31,6 @@
 #include <unistd.h>
 #include <string.h>
 #include <semaphore.h>
-
 
 #define TRUE 1
 
@@ -45,6 +50,12 @@
 #define TRIPULACAO_PIRATA 2 // Define controle da ilha para a tripulacao pirata
 #define MARINHA 3 // Define controle da ilha para a marinha
 
+// Variaveis de combate
+#define FORCA_MIN_TP 1 // forca minima que uma tripulacao de piratas pode ter
+#define FORCA_MAX_TP 5 // forca maxima que uma tripulacao de piratas pode ter
+#define FORCA_MIN_RP 10 // forca minima que uma tripulacao de rei dos piratas pode ter
+#define FORCA_MAX_RP 20 // forca maxima que uma tripulacao de rei dos piratas pode ter
+
 int ilhas_ocupadas = 0; // Quantidade de ilhas ocupadas, o maximo eh definido 
 int trip_rei_pirata = 0, trip_pirata = 0, trip_marinha = 0;
 int estado_ilhas[QTD_ILHAS]; // Vetor que armazena quem esta sob controle da ilha atualmente
@@ -53,8 +64,32 @@ pthread_mutex_t mutex_ilhas = PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t mutex_tripulacao = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t rp_cond = PTHREAD_COND_INITIALIZER;
 pthread_cond_t tp_cond = PTHREAD_COND_INITIALIZER;
-sem_t *sem_ilhas_livres;
 
+// Struct que determina a forca de uma tripulacao pirata
+typedef struct {
+    int id;
+    int forca;
+} atributos_tripulacao;
+
+// Randomiza a forca para as tripulacoes de piratas ou rei piratas
+int inicializa_forca_tripulacoes(int tripulacoes[], int tipo) {
+    int i=0, forca, erro = 0;
+    srand(time(NULL)); 
+    if (tipo == TRIPULACAO_PIRATA) {
+        for (i=0;i<QTD_TPIRATAS;i++) {
+            tripulacoes[i] = ((rand() % FORCA_MAX_TP) + FORCA_MIN_TP); 
+        }
+    } else if (tipo == REI_PIRATA) {
+        for (i=0;i<QTD_REI_PIRATAS;i++) {
+            tripulacoes[i] = ((rand() % FORCA_MAX_RP) + FORCA_MIN_RP); 
+        }
+    } else {
+        erro = 1;
+    }
+    return erro;
+}
+
+// Randomiza uma ilha em que a tripulacao ira chegar a seguir
 int determinar_ilha_para_tripulacao() {
     int ilha=0;
     ilha = (rand() % (QTD_ILHAS)); 
@@ -62,9 +97,11 @@ int determinar_ilha_para_tripulacao() {
 }
 
 void* tripulacao_pirata(void *arg) {
-    int id = *((int *)arg);
+    atributos_tripulacao atributos_tp = *((atributos_tripulacao *)arg);
+    int id = atributos_tp.id;
+    int forca = atributos_tp.forca;
     int ilha_destino = 0;
-    printf("Tripulação pirata id: %d apareceu\n", id);
+    printf("Tripulação pirata id: %d apareceu de forca %d\n", id, forca);
     while(TRUE) {
         // pirata comeca a navegar
         printf("TP %d navegando\n", id);
@@ -100,9 +137,11 @@ void* tripulacao_pirata(void *arg) {
 }
 
 void* rei_pirata(void *arg) {
-    int id = *((int *)arg);
+    atributos_tripulacao atributos_tp = *((atributos_tripulacao *)arg);
+    int id = atributos_tp.id;
+    int forca = atributos_tp.forca;
     int ilha_destino = 0;
-    printf("Rei pirata id: %d apareceu!\n", id);
+    printf("Rei pirata id: %d apareceu de forca %d\n", id, forca);
     while(TRUE) {
         // rei pirata comeca a navegar
         printf("RP %d navegando\n", id);
@@ -150,17 +189,36 @@ void* tripulacao_marinha(void *arg) {
 int main () {
     int i, erro;
     int *id;
+    int forca_trip_piratas[QTD_TPIRATAS];
+    int forca_trip_rei_piratas[QTD_REI_PIRATAS];
     pthread_t tp_threads[QTD_TPIRATAS]; // threads de tripulacoes piratas
     pthread_t rp_threads[QTD_REI_PIRATAS]; // threads de rei dos piratas
     pthread_t tm_threads[QTD_TMARINHEIROS]; // threads de tripulacoes de marinheiros
 
+    atributos_tripulacao atributos;
+
     memset(estado_ilhas, DOMINIO_NEUTRO, sizeof(int)); // inicializa vetor de dominio das ilhas como neutro
+
+    // inicializa forca para piratas
+    erro = inicializa_forca_tripulacoes(forca_trip_piratas, TRIPULACAO_PIRATA);
+    if (erro != 0) {
+        printf("erro ao inicializar as forcas! Saindo\n");
+        exit(0);
+    }
+
+    // inicializa forca para rei dos piratas
+    erro = inicializa_forca_tripulacoes(forca_trip_rei_piratas, REI_PIRATA);
+    if (erro != 0) {
+        printf("erro ao inicializar as forcas! Saindo\n");
+        exit(0);
+    }
 
     // Inicializa as threads dos piratas
     for (i = 0; i < QTD_TPIRATAS; i++) {
-        id = (int *)malloc(sizeof(int));
-        *id = i;
-        erro = pthread_create(&tp_threads[i], NULL, tripulacao_pirata, (void *)(id));
+        atributos_tripulacao *atributos = malloc(sizeof(atributos_tripulacao));
+        atributos->id = i;
+        atributos->forca = forca_trip_piratas[i];
+        erro = pthread_create(&tp_threads[i], NULL, tripulacao_pirata, (void *)(atributos));
         if (erro) {
             printf("erro ao criar a Thread id %d\n", i);
             exit(1);
@@ -169,20 +227,21 @@ int main () {
 
     // Inicializa as threads dos reis dos piratas
     for (i = 0; i < QTD_REI_PIRATAS; i++) {
-        id = (int *)malloc(sizeof(int));
-        *id = i;
-        erro = pthread_create(&rp_threads[i], NULL, rei_pirata, (void *)(id));
+        atributos_tripulacao *atributos = malloc(sizeof(atributos_tripulacao));
+        atributos->id = i;
+        atributos->forca = forca_trip_rei_piratas[i];
+        erro = pthread_create(&rp_threads[i], NULL, rei_pirata, (void *)(atributos));
         if (erro) {
             printf("erro ao criar a Thread id %d\n", i);
             exit(1);
         }
     }
 
-    sem_ilhas_livres = sem_open("/sem_ilhas", O_CREAT, 0644, QTD_ILHAS);
-    if (sem_ilhas_livres == SEM_FAILED) {
-        perror("Falha ao abrir semaforo");
-        exit(-1);
-    }
+    // sem_ilhas_livres = sem_open("/sem_ilhas", O_CREAT, 0644, QTD_ILHAS);
+    // if (sem_ilhas_livres == SEM_FAILED) {
+    //     perror("Falha ao abrir semaforo");
+    //     exit(-1);
+    // }
 
     // Inicializa as threads das tripulacoes de marinheiros
     for (i = 0; i < QTD_TMARINHEIROS; i++) {
